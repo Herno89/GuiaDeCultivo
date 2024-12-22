cb0a292870b87924cf79af823057c8ceb9f0c2b4
Herno <114448348+Herno89@users.noreply.github.com>
2024-12-22 16:30:40 -0300

    updateGuiaDeCultivo

--- data/datosEcomerse.js
+++ data/datosEcomerse.js
@@ -1,21 +1,25 @@
-const base_de_datos = [
-    {
+const productos = {
+    indoor: {
         id:1,
         nombre: "Carpa de cultivo 100x100x200",
         marca: "Dua Tek",
         categoria: "Invernadero",
         precio: "100.00",
+        stock: 20,
+        descuento: 0.1,
         img: "https://www.todomicro.com.ar/10470-large_default/carpa-indoor-de-cultivo-100-x-100-x-200.jpg"
     },
-    {
+    indoor: {
         id:2,
         nombre: "Carpa de cultivo Hulk 120x240x200",
         marca: "Hulk",
         categoria: "Invernadero",
         precio: "180.00",
+        stock: 20,
+        descuento: 0.1,
         img: "https://benditaseagrowshop.com/wp-content/uploads/Carpa-de-cultivo-hulk-120x240-ma-2.jpg"
     },
-    {
+    lampara: {
         id:3,
         nombre: "Lampara de cultivo Cogordo p1000",
         marca: "Cogordo",
@@ -24,28 +28,34 @@ const base_de_datos = [
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_606829-MLU75842971953_042024-F.webp"
 
     },
-    {
+    lampara: {
         id:4,
         nombre: "Panel Led Cultivo Samsung Quantum Board 240w - Ir + Uv",
         marca: "Samsung",
         categoria: "Iluminacion",
         precio: "420.50",
+        stock: 20,
+        descuento: 0.1,
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_789953-MLA74091121612_012024-F.webp"
     },
-    {
+    sustrato: {
         id:5,
         nombre: "GrowMix / Multipro 80 L",
         marca: "Tierra fertil",
         categoria: "Sustratro",
         precio: "18.00",
+        stock: 20,
+        descuento: 0,
         img: "https://juanijuana.com.ar/wp-content/uploads/2021/10/Growmix-Multripro-Sustrato.png"
     },
-    {
+    fertilizante: {
         id:6,
         nombre: "Kit Fertilizante Feeding Grow 50g+ Hybrid 125g + Booster 50g",
         marca: "Green House",
         categoria: "Fertilizantes",
         precio: "27.25",
+        stock: 20,
+        descuento: 0,
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_603998-MLA70610186539_072023-F.webp"
     }
-]
+}
